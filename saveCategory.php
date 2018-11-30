1879f10b81f4c3cb00fdf1f35b65763959932bad
BEN <wahudong@gmail.com>
2018-11-30 17:25:18 -0600

    doing category update

---
+++ saveCategory.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+    if (isset($_SESSION['loginUser'])) {
+        include "connect.php";
+        
+        $id=filter_input(INPUT_POST,'id',FILTER_SANITIZE_NUMBER_INT);
+        $name=filter_input(INPUT_POST,'newCateName', FILTER_SANITIZE_SPECIAL_CHARS);    
+
+       
+
+        $qureyUpdate="UPDATE  Category SET CategoryNAME=:name WHERE CategoryID=:id";
+        $statement=$db->prepare($qureyUpdate);
+        $statement->bindvalue(':id',$id);
+        $statement->bindvalue(':name',$name);
+        $statement->execute();
+        header('Location:categoryManage.php');       
+    }else{
+
+        echo "<script LANGUAGE = 'javascript'> alert('Your can not access this page'); location.href='login.php' </script>";
+    }
+?>
--- updateCategory.php
+++ updateCategory.php
@@ -5,23 +5,29 @@
         include "connect.php";
         
         $id=filter_input(INPUT_GET,'ID',FILTER_SANITIZE_NUMBER_INT);
-    
         
-        
-        $query="SELECT categoryID, categoryName FROM category WHERE categorID==:id";
+        // $id=$_GET['id'];
+        // print_r($_GET);
+        echo '<br>';
+        echo '$id= ';
+        print_r($id);
+        echo '<br>';
+
+        $query="SELECT categoryID, categoryName FROM category WHERE categoryID=:id";
         $statement=$db->prepare($query);
         $statement->bindvalue(':id',$id);
         $statement->execute();
         $categorie=$statement->fetch();
 
-       
+       print_r($categorie);
+  
 
-        $qureyUpdate="UPDATE  Category SET CategorynAME=:name WHERE CategoryID=:id";
-        $statement=$db->prepare($qureyUpdate);
-        $statement->bindvalue(':id',$id);
-        $statement->bindvalue(':name',$name);
-        $statement->execute();
-        header('Location:categoryManage.php');       
+        // $qureyUpdate="UPDATE  Category SET CategorynAME=:name WHERE CategoryID=:id";
+        // $statement=$db->prepare($qureyUpdate);
+        // $statement->bindvalue(':id',$id);
+        // $statement->bindvalue(':name',$name);
+        // $statement->execute();
+        // header('Location:categoryManage.php');       
     }else{
 
         echo "<script LANGUAGE = 'javascript'> alert('Your can not access this page'); location.href='login.php' </script>";
@@ -35,10 +41,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet" type="text/css" media="screen" href="" />
     <script src=""></script>
 </head>
 <body>
-    
+    <h3>Please Enter The New Name to The Category</h3>
+<form action="saveCategory.php" method="POST" name="updateCategory" id="updateCategory">
+<input type="hidden" name="id" value=<?=$id?>>
+<label for="newCateName">CategoryID: <?=$categorie['categoryID']?></label>
+<input type="text" name="newCateName" id="newCatName" value=<?=$categorie['categoryName']?>>
+<input type="submit" name="submitUpdate" value="Save">
+
+</form>
+
 </body>
 </html>
